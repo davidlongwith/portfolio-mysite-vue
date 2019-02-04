52a926b2da857034ff8b1399e8506d64dc50cc5c
David Longwith <david@davidlongwith.com>
2019-02-04 13:18:13 -0500

    add javascript drawer code

--- src/main.js
+++ src/main.js
@@ -9,3 +9,40 @@ new Vue({
   router,
   render: h => h(App)
 }).$mount("#app");
+
+/*
+ *
+ * Side Drawers
+ *
+ */
+
+// Get buttons and drawers
+const menuBttn = document.querySelectorAll(".menu-button");
+const menu = document.querySelector("#main-menu");
+
+const contactBttn = document.querySelectorAll(".contact-button");
+const contactDiv = document.querySelector("#contact-div");
+
+const main = document.getElementsByTagName("main")[0];
+
+// click button(s) to show menu
+for (let i = 0; i < menuBttn.length; i++) {
+  menuBttn[i].addEventListener("click", function(e) {
+    menu.classList.toggle("open");
+    e.stopPropagation();
+  });
+}
+
+// click button(s) to show contact form
+for (let i = 0; i < contactBttn.length; i++) {
+  contactBttn[i].addEventListener("click", function(e) {
+    contactDiv.classList.toggle("open");
+    e.stopPropagation();
+  });
+}
+
+// click outside to close
+main.addEventListener("click", function() {
+  menu.classList.remove("open");
+  contactDiv.classList.remove("open");
+});
