feaed64a93a7be57e05691081e82b7a57d04c4ee
David Longwith <david@davidlongwith.com>
2019-02-28 19:09:32 -0500

    add projects view and add to menu

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -2,6 +2,7 @@
   <nav>
     <router-link :to="{ name: 'home' }">Home</router-link>
     <router-link :to="{ name: 'about' }">About</router-link>
+    <router-link :to="{ name: 'projects' }">Projects</router-link>
   </nav>
 </template>
 
